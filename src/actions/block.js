922626cdfb4c24e9e11e06e49c74f9f9c15dd150
Runchao Han <runchao.han@monash.edu>
2021-06-22 22:56:56 +1000

    init

--- src/actions/block.js
+++ src/actions/block.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { applyCORSToUrl, unixToDateString } from "../utils";
-import { FETCH_BLOCK } from "../constants/types";
-import { URL_BLOCK } from "../constants/api";
+import {applyCORSToUrl, unixToDateString} from "../utils";
+import {FETCH_BLOCK} from "../constants/types";
+import {URL_BLOCK, URL_LOCAL_NODE} from "../constants/api";
 
 const normalizeBlock = block => ({
-  transactions: block.tx.map(({ weight, time, hash }) => ({
+  transactions: block.tx.map(({weight, time, hash}) => ({
     weight,
     time: unixToDateString(time),
     hash
@@ -29,11 +29,18 @@ const normalizeBlock = block => ({
 
 export const fetchBlock = hash => {
   return dispatch => {
-    const url = applyCORSToUrl(`${URL_BLOCK}/${hash}`);
+    // const url = applyCORSToUrl(`${URL_BLOCK}/${hash}`);
+    const url = URL_LOCAL_NODE;
+    const data = JSON.stringify({
+      "jsonrpc": "2.0",
+      "method": "getblock",
+      "params": [hash, true],
+      "id": 1
+    });
     return axios
       .get(url)
       .then(({data}) => {
-        dispatch({ type: FETCH_BLOCK, payload: normalizeBlock(data) });
+        dispatch({type: FETCH_BLOCK, payload: normalizeBlock(data)});
       })
       .catch(error => {
         throw error;
