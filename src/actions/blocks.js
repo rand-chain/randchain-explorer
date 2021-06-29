a86daf3214b1fd8117e031999721f77979002fb9
Runchao Han <runchao.han@monash.edu>
2021-06-29 21:53:09 +1000

    init

--- src/actions/blocks.js
+++ src/actions/blocks.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { applyCORSToUrl, unixToDateString } from "../utils";
-import { FETCH_BLOCKS } from "../constants/types";
-import { URL_BLOCKS } from '../constants/api';
+import {applyCORSToUrl, unixToDateString} from "../utils";
+import {FETCH_BLOCKS} from "../constants/types";
+import {URL_LOCAL_NODE} from "../constants/api";
 
-const normalizeBlocks = ({ blocks }) =>
-  blocks.map(({ height, time, hash }) => ({
+const normalizeBlocks = ({blocks}) =>
+  blocks.map(({height, time, hash}) => ({
     height,
     time: unixToDateString(time),
     hash
@@ -12,9 +12,24 @@ const normalizeBlocks = ({ blocks }) =>
 
 export const fetchBlocks = (timestamp = new Date().getTime()) => {
   return dispatch => {
-    const url = applyCORSToUrl(`${URL_BLOCKS}/${timestamp}`);
-    return axios
-      .get(url)
+    // const url = applyCORSToUrl(`${URL_BLOCKS}/${timestamp}`);
+
+    const data = JSON.stringify({
+      "jsonrpc": "2.0",
+      "method": "getblocks",
+      "params": [0],
+      "id": 1
+    });
+    var config = {
+      method: 'post',
+      url: URL_LOCAL_NODE,
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: data
+    };
+
+    return axios(config)
       .then(({data}) => {
         dispatch({
           type: FETCH_BLOCKS,
